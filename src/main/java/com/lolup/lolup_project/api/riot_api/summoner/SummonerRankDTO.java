e714d22ace6889ec0140019a0a2af3fb03928f17
khj1 <74335027+khj1@users.noreply.github.com>
2021-08-16 20:48:52 +0900

    fix : riot-api 데이터 파싱 내용 수정
    unranked된 유저의 정보가 제대로 파싱되지 않는 문제를 해결했습니다.

--- src/main/java/com/lolup/lolup_project/api/riot_api/summoner/SummonerRankDTO.java
+++ src/main/java/com/lolup/lolup_project/api/riot_api/summoner/SummonerRankDTO.java
@@ -1,23 +1,18 @@
 package com.lolup.lolup_project.api.riot_api.summoner;
 
-import lombok.Data;
+import lombok.*;
 
-@Data
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SummonerRankDTO {
 
-    private Long summonerLevel;
     private int profileIconId;
-    private String leagueId;
-    private String summonerId;
     private String summonerName;
-    private String queueType;
     private String tier;
     private String rank;
-    private int leaguePoints;
     private int wins;
     private int losses;
-    private boolean hotStreak;
-    private boolean veteran;
-    private boolean freshBlood;
-    private boolean inactive;
+
 }
