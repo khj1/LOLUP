931ebdd982c75e34de2694ddf5a027cacab848b6
khj1 <74335027+khj1@users.noreply.github.com>
2021-08-16 20:55:11 +0900

    fix : riot-api 데이터 파싱 내용 수정 (#27)
    unranked된 유저의 정보가 제대로 파싱되지 않는 문제를 해결했습니다.

--- src/main/java/com/lolup/lolup_project/api/riot_api/summoner/SummonerController.java
+++ src/main/java/com/lolup/lolup_project/api/riot_api/summoner/SummonerController.java
@@ -5,30 +5,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class SummonerController {
 
     private final SummonerService summonerService;
 
-    @GetMapping("/summoner/account/{summonerName}")
-    public String getRankInfo(@PathVariable String summonerName, Model model) {
-        model.addAttribute("info", summonerService.getSummonerTotalSoloRankInfo(summonerName));
-        model.addAttribute("version", summonerService.getGameVersion()[0]);
-        return "summonerInfo";
+    @GetMapping("/riot/{summonerName}")
+    public Map<String, Object> getLatestSoloRankHistories(@PathVariable String summonerName) {
+        return summonerService.getSummonerSummaryInfo(summonerName);
     }
-
-    @GetMapping("/summoner/match/{summonerName}")
-    public String getLatestSoloRankHistories(@PathVariable String summonerName, Model model) {
-        Map<String, Object> map = summonerService.getSummonerSummaryInfo(summonerName);
-        model.addAttribute("version", map.get("version"));
-        model.addAttribute("totalInfo", map.get("totalInfo"));
-        model.addAttribute("latestWinRate", map.get("winRateOfLatestGames"));
-        model.addAttribute("most3", map.get("most3ChampOfLatestGames"));
-
-        return "summonerInfo";
-    }
-
 }
