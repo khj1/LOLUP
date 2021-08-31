31ddd457553288bc90620c3df695c88de88ff542
khj1 <74335027+khj1@users.noreply.github.com>
2021-09-01 01:17:17 +0900

    fix : delete cookie setDomain()

--- src/main/java/com/lolup/lolup_project/config/WebConfig.java
+++ src/main/java/com/lolup/lolup_project/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.lolup.lolup_project.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
