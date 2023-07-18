c0e4cca49f5aced708a7ec74963a379736f874ff
khj1 <74335027+khj1@users.noreply.github.com>
2023-07-18 19:18:40 +0900

    test: 리프레시 토큰 조회 기능 테스트

--- src/main/java/com/lolup/lolup_project/token/RefreshTokenRepository.java
+++ src/main/java/com/lolup/lolup_project/token/RefreshTokenRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.lolup.lolup_project.member.Member;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-	void deleteByMember(final Member member);
-
 	Optional<RefreshToken> findByRefreshToken(final String refreshToken);
+
+	void deleteByMember(final Member member);
 }
