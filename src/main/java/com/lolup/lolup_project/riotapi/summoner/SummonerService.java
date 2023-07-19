23d26cc32d18ce804a55b599a53bce0ac3fd3150
khj1 <74335027+khj1@users.noreply.github.com>
2023-07-19 15:51:40 +0900

    fix: 라이엇 API secret key 암호화

--- src/main/java/com/lolup/lolup_project/riotapi/summoner/SummonerService.java
+++ src/main/java/com/lolup/lolup_project/riotapi/summoner/SummonerService.java
@@ -1,200 +1,210 @@
 package com.lolup.lolup_project.riotapi.summoner;
 
-import com.lolup.lolup_project.riotapi.APIConst;
-import com.lolup.lolup_project.riotapi.match.MatchDto;
-import com.lolup.lolup_project.riotapi.match.MatchInfoDto;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import com.lolup.lolup_project.riotapi.match.MatchDto;
+import com.lolup.lolup_project.riotapi.match.MatchInfoDto;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class SummonerService {
 
-    private final WebClient webClient;
+	private final WebClient webClient;
 
-    public SummonerAccountDto getAccountInfo(String summonerName) {
-        return webClient
-                .get()
-                .uri("/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=" + APIConst.riot_apiKey)
-                .retrieve()
-                .bodyToMono(SummonerAccountDto.class)
-                .block();
-    }
+	@Value("${security.riot.api-key}")
+	private String apiKey;
 
-    private SummonerRankInfo getSummonerTotalSoloRankInfo(String summonerName) {
-        SummonerAccountDto summonerAccountInfo = getAccountInfo(summonerName);
-        String id = summonerAccountInfo.getId();
-        int iconId = summonerAccountInfo.getProfileIconId();
+	public SummonerAccountDto getAccountInfo(String summonerName) {
+		return webClient
+				.get()
+				.uri("/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=" + apiKey)
+				.retrieve()
+				.bodyToMono(SummonerAccountDto.class)
+				.block();
+	}
 
-        SummonerRankInfoDto summonerRankInfoDto = getRankInfoDto(id);
+	private SummonerRankInfo getSummonerTotalSoloRankInfo(String summonerName) {
+		SummonerAccountDto summonerAccountInfo = getAccountInfo(summonerName);
+		String id = summonerAccountInfo.getId();
+		int iconId = summonerAccountInfo.getProfileIconId();
 
-        if (summonerRankInfoDto == null) {
-            return getUnrankedInfo(summonerName);
-        }
+		SummonerRankInfoDto summonerRankInfoDto = getRankInfoDto(id);
 
-        return getRankInfo(summonerRankInfoDto, iconId);
-    }
+		if (summonerRankInfoDto == null) {
+			return getUnrankedInfo(summonerName);
+		}
 
-    private SummonerRankInfo getRankInfo(SummonerRankInfoDto summonerRankInfoDto, int iconId) {
-        return SummonerRankInfo.builder()
-                .summonerName(summonerRankInfoDto.getSummonerName())
-                .tier(summonerRankInfoDto.getTier())
-                .rank(summonerRankInfoDto.getRank())
-                .wins(summonerRankInfoDto.getWins())
-                .losses(summonerRankInfoDto.getLosses())
-                .iconId(iconId)
-                .build();
-    }
+		return getRankInfo(summonerRankInfoDto, iconId);
+	}
 
-    private SummonerRankInfo getUnrankedInfo(String summonerName) {
-        return SummonerRankInfo.builder()
-                .summonerName(summonerName)
-                .tier("UNRANKED")
-                .rank("언랭크")
-                .wins(0)
-                .losses(0)
-                .build();
-    }
+	private SummonerRankInfo getRankInfo(SummonerRankInfoDto summonerRankInfoDto, int iconId) {
+		return SummonerRankInfo.builder()
+				.summonerName(summonerRankInfoDto.getSummonerName())
+				.tier(summonerRankInfoDto.getTier())
+				.rank(summonerRankInfoDto.getRank())
+				.wins(summonerRankInfoDto.getWins())
+				.losses(summonerRankInfoDto.getLosses())
+				.iconId(iconId)
+				.build();
+	}
 
-    private SummonerRankInfoDto getRankInfoDto(String id) {
-        return webClient
-                .get()
-                .uri("/lol/league/v4/entries/by-summoner/" + id + "?api_key=" + APIConst.riot_apiKey)
-                .retrieve()
-                .bodyToFlux(SummonerRankInfoDto.class)
-                .blockFirst();
-    }
+	private SummonerRankInfo getUnrankedInfo(String summonerName) {
+		return SummonerRankInfo.builder()
+				.summonerName(summonerName)
+				.tier("UNRANKED")
+				.rank("언랭크")
+				.wins(0)
+				.losses(0)
+				.build();
+	}
 
-    public String getGameVersion() {
-        String[] versions = webClient
-                .get()
-                .uri("https://ddragon.leagueoflegends.com/api/versions.json")
-                .retrieve()
-                .bodyToMono(String[].class)
-                .block();
+	private SummonerRankInfoDto getRankInfoDto(String id) {
+		return webClient
+				.get()
+				.uri("/lol/league/v4/entries/by-summoner/" + id + "?api_key=" + apiKey)
+				.retrieve()
+				.bodyToFlux(SummonerRankInfoDto.class)
+				.blockFirst();
+	}
 
-        return versions[0];
-    }
+	public String getGameVersion() {
+		String[] versions = webClient
+				.get()
+				.uri("https://ddragon.leagueoflegends.com/api/versions.json")
+				.retrieve()
+				.bodyToMono(String[].class)
+				.block();
 
-    private List<MatchInfoDto> getMatchInfos(String summonerName) {
+		return versions[0];
+	}
 
-        List<MatchInfoDto> matchInfoDtos = new ArrayList<>();
-        String[] matchIds = getMatchIds(summonerName);
+	private List<MatchInfoDto> getMatchInfos(String summonerName) {
 
-        Arrays.stream(matchIds).forEach(matchId -> matchInfoDtos.add(getMatchDto(matchId).getInfo()));
+		List<MatchInfoDto> matchInfoDtos = new ArrayList<>();
+		String[] matchIds = getMatchIds(summonerName);
 
-        return matchInfoDtos;
-    }
+		Arrays.stream(matchIds).forEach(matchId -> matchInfoDtos.add(getMatchDto(matchId).getInfo()));
 
-    private String getLatestWinRate(String summonerName, List<MatchInfoDto> matchInfoDtos) {
-        long winCount = matchInfoDtos.stream()
-                .filter(matchInfo -> getWin(summonerName, matchInfo)).count();
+		return matchInfoDtos;
+	}
 
-        return ((double)winCount / 30) * 100 + "%";
-    }
+	private String getLatestWinRate(String summonerName, List<MatchInfoDto> matchInfoDtos) {
+		long winCount = matchInfoDtos.stream()
+				.filter(matchInfo -> getWin(summonerName, matchInfo)).count();
 
-    private List<MostInfo> getLatestMost3(String summonerName, List<MatchInfoDto> matches) {
-        Map<String, Integer> mostsIn30Games = getMostsIn30Games(summonerName, matches);
-        List<Map.Entry<String, Integer>> sortedMosts = getSortedMosts(mostsIn30Games);
-        List<Map.Entry<String, Integer>> most3Entries = getMost3Entries(sortedMosts);
+		return ((double)winCount / 30) * 100 + "%";
+	}
 
-        return getMost3List(most3Entries);
-    }
+	private List<MostInfo> getLatestMost3(String summonerName, List<MatchInfoDto> matches) {
+		Map<String, Integer> mostsIn30Games = getMostsIn30Games(summonerName, matches);
+		List<Map.Entry<String, Integer>> sortedMosts = getSortedMosts(mostsIn30Games);
+		List<Map.Entry<String, Integer>> most3Entries = getMost3Entries(sortedMosts);
 
-    private List<MostInfo> getMost3List(List<Map.Entry<String, Integer>> entries) {
-        List<MostInfo> mostInfos = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : entries) {
-            MostInfo mostInfo = MostInfo.create(entry.getKey(), entry.getValue());
-            mostInfos.add(mostInfo);
-        }
-        return mostInfos;
-    }
+		return getMost3List(most3Entries);
+	}
 
-    private List<Map.Entry<String, Integer>> getMost3Entries(List<Map.Entry<String, Integer>> sortedMosts) {
-        if (sortedMosts.size() >= 3) {
-            return sortedMosts.subList(0, 3);
-        }
-        if (sortedMosts.size() == 2) {
-            return sortedMosts.subList(0, 2);
-        }
-        return sortedMosts.subList(0, 1);
-    }
+	private List<MostInfo> getMost3List(List<Map.Entry<String, Integer>> entries) {
+		List<MostInfo> mostInfos = new ArrayList<>();
+		for (Map.Entry<String, Integer> entry : entries) {
+			MostInfo mostInfo = MostInfo.create(entry.getKey(), entry.getValue());
+			mostInfos.add(mostInfo);
+		}
+		return mostInfos;
+	}
 
-    private List<Map.Entry<String, Integer>> getSortedMosts(Map<String, Integer> mostsIn30Games) {
-        return mostsIn30Games
-                .entrySet().stream()
-                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
-                .collect(Collectors.toList());
-    }
+	private List<Map.Entry<String, Integer>> getMost3Entries(List<Map.Entry<String, Integer>> sortedMosts) {
+		if (sortedMosts.size() >= 3) {
+			return sortedMosts.subList(0, 3);
+		}
+		if (sortedMosts.size() == 2) {
+			return sortedMosts.subList(0, 2);
+		}
+		return sortedMosts.subList(0, 1);
+	}
 
-    private Map<String, Integer> getMostsIn30Games(String summonerName, List<MatchInfoDto> matchInfoDtos) {
-        Map<String, Integer> mostsIn30Games = new HashMap<>();
+	private List<Map.Entry<String, Integer>> getSortedMosts(Map<String, Integer> mostsIn30Games) {
+		return mostsIn30Games
+				.entrySet().stream()
+				.sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+				.collect(Collectors.toList());
+	}
 
-        for (MatchInfoDto matchInfoDto : matchInfoDtos) {
-            String championName = getChampionName(summonerName, matchInfoDto);
-            mostsIn30Games.put(championName, addChampionPlayCount(mostsIn30Games, championName));
-        }
+	private Map<String, Integer> getMostsIn30Games(String summonerName, List<MatchInfoDto> matchInfoDtos) {
+		Map<String, Integer> mostsIn30Games = new HashMap<>();
 
-        return mostsIn30Games;
-    }
+		for (MatchInfoDto matchInfoDto : matchInfoDtos) {
+			String championName = getChampionName(summonerName, matchInfoDto);
+			mostsIn30Games.put(championName, addChampionPlayCount(mostsIn30Games, championName));
+		}
 
-    private Integer addChampionPlayCount(Map<String, Integer> mostsIn30Games, String championName) {
-        if (mostsIn30Games.containsKey(championName)) {
-            return mostsIn30Games.get(championName) + 1;
-        }
-        return 1;
-    }
+		return mostsIn30Games;
+	}
 
-    private String[] getMatchIds(String summonerName) {
-        String puuid = getAccountInfo(summonerName).getPuuid();
-        return webClient
-                .get()
-                .uri("https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?api_key=" + APIConst.riot_apiKey + "&start=0&count=30")
-                .retrieve()
-                .bodyToMono(String[].class)
-                .block();
-    }
+	private Integer addChampionPlayCount(Map<String, Integer> mostsIn30Games, String championName) {
+		if (mostsIn30Games.containsKey(championName)) {
+			return mostsIn30Games.get(championName) + 1;
+		}
+		return 1;
+	}
 
-    public SummonerDto find(String summonerName) {
+	private String[] getMatchIds(String summonerName) {
+		String puuid = getAccountInfo(summonerName).getPuuid();
+		return webClient
+				.get()
+				.uri("https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?api_key="
+						+ apiKey + "&start=0&count=30")
+				.retrieve()
+				.bodyToMono(String[].class)
+				.block();
+	}
 
-        SummonerRankInfo info = getSummonerTotalSoloRankInfo(summonerName);
-        List<MatchInfoDto> matchInfoDtos = getMatchInfos(summonerName);
-        String latestWinRate = getLatestWinRate(summonerName, matchInfoDtos);
-        List<MostInfo> most3 = getLatestMost3(summonerName, matchInfoDtos);
+	public SummonerDto find(String summonerName) {
 
-        return new SummonerDto(latestWinRate, info, most3);
-    }
+		SummonerRankInfo info = getSummonerTotalSoloRankInfo(summonerName);
+		List<MatchInfoDto> matchInfoDtos = getMatchInfos(summonerName);
+		String latestWinRate = getLatestWinRate(summonerName, matchInfoDtos);
+		List<MostInfo> most3 = getLatestMost3(summonerName, matchInfoDtos);
 
-    private MatchDto getMatchDto(String matchId) {
-        return webClient
-                .get()
-                .uri("https://asia.api.riotgames.com/lol/match/v5/matches/" + matchId + "?api_key=" + APIConst.riot_apiKey)
-                .retrieve()
-                .bodyToMono(MatchDto.class)
-                .block();
-    }
+		return new SummonerDto(latestWinRate, info, most3);
+	}
 
-    private Boolean getWin(String summonerName, MatchInfoDto matchInfoDto) {
-        return matchInfoDto
-                .getParticipants()
-                .stream().filter(participantDto -> participantDto.getSummonerName().equals(summonerName))
-                .collect(Collectors.toList())
-                .get(0)
-                .isWin();
-    }
+	private MatchDto getMatchDto(String matchId) {
+		return webClient
+				.get()
+				.uri("https://asia.api.riotgames.com/lol/match/v5/matches/" + matchId + "?api_key=" + apiKey)
+				.retrieve()
+				.bodyToMono(MatchDto.class)
+				.block();
+	}
 
-    private String getChampionName(String summonerName, MatchInfoDto matchInfoDto) {
-        return matchInfoDto
-                .getParticipants()
-                .stream().filter(participantDto -> participantDto.getSummonerName().equals(summonerName))
-                .collect(Collectors.toList())
-                .get(0)
-                .getChampionName();
-    }
+	private Boolean getWin(String summonerName, MatchInfoDto matchInfoDto) {
+		return matchInfoDto
+				.getParticipants()
+				.stream().filter(participantDto -> participantDto.getSummonerName().equals(summonerName))
+				.collect(Collectors.toList())
+				.get(0)
+				.isWin();
+	}
+
+	private String getChampionName(String summonerName, MatchInfoDto matchInfoDto) {
+		return matchInfoDto
+				.getParticipants()
+				.stream().filter(participantDto -> participantDto.getSummonerName().equals(summonerName))
+				.collect(Collectors.toList())
+				.get(0)
+				.getChampionName();
+	}
 }
