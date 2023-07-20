2088f24e45739372fdfe25c18eb122666bde17f4
khj1 <74335027+khj1@users.noreply.github.com>
2023-07-20 15:03:08 +0900

    test: 라이엇 계정 정보 호출 기능 테스트

---
+++ src/test/java/com/lolup/lolup_project/riotapi/summoner/SummonerServiceTest.java
@@ -0,0 +1,77 @@
+package com.lolup.lolup_project.riotapi.summoner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+
+@SpringBootTest
+class SummonerServiceTest {
+
+	private static final String MOCK_SERVER_BASE_URL = "http://localhost:%s";
+
+	private static MockWebServer mockWebServer;
+	private static SummonerService summonerService;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@BeforeAll
+	static void setUp() throws IOException {
+		mockWebServer = new MockWebServer();
+		mockWebServer.start();
+
+		String baseUrl = String.format(MOCK_SERVER_BASE_URL, mockWebServer.getPort());
+		WebClient webClient = WebClient.create(baseUrl);
+		summonerService = new SummonerService(webClient);
+	}
+
+	@AfterAll
+	static void tearDown() throws IOException {
+		mockWebServer.shutdown();
+	}
+
+	@DisplayName("라이엇 계정 정보를 불러온다.")
+	@Test
+	void getAccountInfo() throws JsonProcessingException {
+		SummonerAccountDto 라이엇_계정_정보_응답 = createSummonerAccountDto();
+
+		mockWebServer.enqueue(new MockResponse()
+				.addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+				.addHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+				.setBody(objectMapper.writeValueAsString(라이엇_계정_정보_응답)));
+
+		SummonerAccountDto API_호출_결과 = summonerService.getAccountInfo("testName");
+
+		assertThat(API_호출_결과)
+				.usingRecursiveComparison()
+				.isEqualTo(라이엇_계정_정보_응답);
+	}
+
+	private SummonerAccountDto createSummonerAccountDto() {
+		return SummonerAccountDto.builder()
+				.id("id")
+				.name("testName")
+				.puuId("puuId")
+				.accountId("accountId")
+				.revisionDate(1L)
+				.summonerLevel(1L)
+				.profileIconId(1)
+				.build();
+	}
+}
