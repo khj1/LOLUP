47dcaa2f57ff2c2246d1ab6c0a0a53b105519e0e
khj1 <74335027+khj1@users.noreply.github.com>
2023-07-24 13:48:43 +0900

    test: DuoRepository 테스트 수정

--- src/test/java/com/lolup/lolup_project/duo/domain/DuoRepositoryTest.java
+++ src/test/java/com/lolup/lolup_project/duo/domain/DuoRepositoryTest.java
@@ -1,10 +1,12 @@
 package com.lolup.lolup_project.duo.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,6 +14,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
+import com.lolup.lolup_project.config.JpaAuditingConfig;
 import com.lolup.lolup_project.config.QuerydslConfig;
 import com.lolup.lolup_project.duo.application.dto.DuoDto;
 import com.lolup.lolup_project.duo.application.dto.SummonerDto;
@@ -21,7 +24,7 @@ import com.lolup.lolup_project.riot.summoner.domain.MostInfo;
 
 import jakarta.persistence.EntityManager;
 
-@Import(QuerydslConfig.class)
+@Import({QuerydslConfig.class, JpaAuditingConfig.class})
 @DataJpaTest
 class DuoRepositoryTest {
 
@@ -34,107 +37,62 @@ class DuoRepositoryTest {
 	@Autowired
 	private MemberRepository memberRepository;
 
+	@DisplayName("필터를 통해 듀오를 조회한다.")
 	@Test
-	public void 듀오_추가_테스트() {
-		//given
-		Duo duo = getDuo(SummonerTier.BRONZE, SummonerPosition.BOT);
+	void findAll() {
+		duoRepository.save(createDuo(SummonerTier.GOLD, SummonerPosition.JUG));
+		duoRepository.save(createDuo(SummonerTier.GOLD, SummonerPosition.TOP));
+		duoRepository.save(createDuo(SummonerTier.SILVER, SummonerPosition.JUG));
+		duoRepository.save(createDuo(SummonerTier.SILVER, SummonerPosition.TOP));
 
-		//when
-		duoRepository.save(duo);
-
-		//then
-		Duo findDuo = duoRepository.findById(duo.getId()).orElse(null);
-		assertThat(duo.getDesc()).isEqualTo(findDuo.getDesc());
-		assertThat(duo.getId()).isEqualTo(findDuo.getId());
-	}
-
-	@Test
-	void 듀오_필터적용_조회() {
-		// given
-		duoRepository.save(getDuo(SummonerTier.GOLD, SummonerPosition.JUG));
-		duoRepository.save(getDuo(SummonerTier.GOLD, SummonerPosition.TOP));
-		duoRepository.save(getDuo(SummonerTier.SILVER, SummonerPosition.JUG));
-		duoRepository.save(getDuo(SummonerTier.SILVER, SummonerPosition.TOP));
-
-		// when
 		PageRequest page = PageRequest.of(0, 20);
 
-		Page<DuoDto> gold_jug = duoRepository.findAll(SummonerPosition.JUG, SummonerTier.GOLD, page);
+		Page<DuoDto> goldJug = duoRepository.findAll(SummonerPosition.JUG, SummonerTier.GOLD, page);
 		Page<DuoDto> gold = duoRepository.findAll(null, SummonerTier.GOLD, page);
 		Page<DuoDto> all = duoRepository.findAll(null, null, page);
 
-		List<DuoDto> list_gold_jug = gold_jug.getContent();
-		List<DuoDto> list_gold = gold.getContent();
-		List<DuoDto> list_all = all.getContent();
+		long sizeOfGoldJug = goldJug.getNumberOfElements();
+		long sizeOfGold = gold.getNumberOfElements();
+		long totalSize = all.getTotalElements();
 
-		long size_gold_jug = gold_jug.getNumberOfElements();
-		long count_gold_jug = list_gold_jug.stream()
-				.filter(dto ->
-						dto.getPosition().equals(SummonerPosition.JUG) &&
-								dto.getTier().equals(SummonerTier.GOLD)
-				)
-				.count();
-
-		long size_gold = gold.getNumberOfElements();
-		long count_gold = list_gold.stream()
-				.filter(dto -> dto.getTier().equals(SummonerTier.GOLD))
-				.count();
-
-		long size_all = all.getTotalElements();
-
-		//then
-		assertThat(size_gold_jug).isEqualTo(count_gold_jug).isLessThan(size_all);
-		assertThat(size_gold).isEqualTo(count_gold).isLessThan(size_all);
+		assertAll(
+				() -> assertThat(sizeOfGoldJug).isEqualTo(1),
+				() -> assertThat(sizeOfGold).isEqualTo(2),
+				() -> assertThat(totalSize).isEqualTo(4)
+		);
 	}
 
+	@DisplayName("듀오 수정 시 수정일자가 올바르게 나온다.")
 	@Test
-	void 데이터_수정() {
-		//given
-		Long beforeId = duoRepository.save(getDuo(SummonerTier.UNRANKED, SummonerPosition.MID)).getId();
+	void update() {
+		Duo duo = createDuo(SummonerTier.UNRANKED, SummonerPosition.MID);
+		Long memberId = duoRepository.save(duo).getId();
 
+		duo.update(SummonerPosition.BOT, "updated");
 		em.flush();
-		em.clear();
 
-		//when
-		duoRepository.findById(beforeId).orElse(null).update(SummonerPosition.BOT, "updated");
+		Duo findDuo = duoRepository.findById(memberId)
+				.orElseThrow();
 
-		em.flush();
-		em.clear();
-
-		Duo findDuo = duoRepository.findById(beforeId).orElse(null);
-
-		//then
-		assertThat(findDuo.getDesc()).isEqualTo("updated");
-		assertThat(findDuo.getPosition()).isEqualTo(SummonerPosition.BOT);
+		assertThat(findDuo.getLastModifiedDate()).isAfter(findDuo.getCreatedDate());
 	}
 
+	@DisplayName("듀오 ID와 멤버 ID로 듀오를 삭제한다.")
 	@Test
-	public void 데이터_삭제() throws Exception {
-		//given
-		Duo duo = getDuo(SummonerTier.UNRANKED, SummonerPosition.SUP);
+	void delete() {
+		Duo duo = createDuo(SummonerTier.UNRANKED, SummonerPosition.SUP);
 		duoRepository.save(duo);
 
 		Long duoId = duo.getId();
 		Long memberId = duo.getMember().getId();
 
-		//when
-		em.flush();
-		em.clear();
-
 		duoRepository.deleteByIdAndMemberId(duoId, memberId);
 
-		em.flush();
-		em.clear();
-
-		//then
-		assertThat(duoRepository.findById(duoId).orElse(null)).isNull();
+		assertThat(duoRepository.findById(duoId)).isEmpty();
 	}
 
-	private Duo getDuo(final SummonerTier tier, final SummonerPosition position) {
-		Member member = Member.builder()
-				.name(position.name() + " " + tier.name())
-				.build();
-
+	private Duo createDuo(final SummonerTier tier, final SummonerPosition position) {
+		Member member = createMember(tier, position);
 		memberRepository.save(member);
 
 		List<MostInfo> most3 = new ArrayList<>();
@@ -142,16 +100,25 @@ class DuoRepositoryTest {
 		most3.add(MostInfo.create("Lucian", 3L));
 		most3.add(MostInfo.create("Zed", 2L));
 
-		SummonerRankInfo info = SummonerRankInfo.builder()
+		SummonerRankInfo info = createSummonerRankInfo(tier);
+		SummonerDto summonerDto = new SummonerDto(100, 0.2d, info, most3);
+
+		return Duo.create(member, summonerDto, position, "hi");
+	}
+
+	private Member createMember(final SummonerTier tier, final SummonerPosition position) {
+		return Member.builder()
+				.name(position.name() + " " + tier.name())
+				.build();
+	}
+
+	private SummonerRankInfo createSummonerRankInfo(final SummonerTier tier) {
+		return SummonerRankInfo.builder()
 				.summonerName("summonerName")
 				.rank(SummonerRank.III)
 				.tier(tier)
 				.wins(100)
 				.losses(100)
 				.build();
-
-		SummonerDto summonerDto = new SummonerDto(100, 0.2d, info, most3);
-
-		return Duo.create(member, summonerDto, position, "hi");
 	}
 }
