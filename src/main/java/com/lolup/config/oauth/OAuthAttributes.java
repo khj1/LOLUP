d07f1ee383990468ebc3359f84b6fa884974c29d
khj1 <74335027+khj1@users.noreply.github.com>
2023-07-29 13:43:18 +0900

    fix: unchecked cast 경고 제거(@SuppressWarnings)

--- src/main/java/com/lolup/config/oauth/OAuthAttributes.java
+++ src/main/java/com/lolup/config/oauth/OAuthAttributes.java
@@ -15,14 +15,19 @@ public enum OAuthAttributes {
 				(String)attributes.get("picture"),
 				SocialType.GOOGLE);
 	}),
+
+	@SuppressWarnings("unchecked") //DefaultOAuth2User의 attributes 자료형이 Map<String, Object>이므로 형 안정성을 유지한다.
 	NAVER("naver", (attributes) -> {
 		Map<String, Object> response = (Map<String, Object>)attributes.get("response");
+
 		return new UserProfile(
 				(String)response.get("name"),
 				(String)response.get("email"),
 				(String)response.get("picture"),
 				SocialType.NAVER);
 	}),
+
+	@SuppressWarnings("unchecked")
 	KAKAO("kakao", (attributes) -> {
 		Map<String, Object> kakaoAccount = (Map<String, Object>)attributes.get("kakao_account");
 		Map<String, Object> kakaoProfile = (Map<String, Object>)kakaoAccount.get("profile");
