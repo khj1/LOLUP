51c787d833b2bd7b82f61488f33f3b25d5adde3d
김한준 (Hanjun Kim) <dhepd8849@gmail.com>
2023-08-10 11:29:33 +0900

    fix: 라이엇 API 비동기 호출을 통한 성능 개선

--- src/main/java/com/lolup/riot/match/application/MatchService.java
+++ src/main/java/com/lolup/riot/match/application/MatchService.java
@@ -8,7 +8,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -18,6 +17,9 @@ import com.lolup.riot.match.application.dto.ParticipantDto;
 import com.lolup.riot.match.application.dto.RecentMatchStatsDto;
 import com.lolup.riot.match.exception.InvalidMatchIdException;
 import com.lolup.riot.match.exception.InvalidPuuIdException;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class MatchService {
@@ -36,45 +38,50 @@ public class MatchService {
 	}
 
 	public RecentMatchStatsDto requestRecentMatchStats(final String puuId) {
-		List<String> matchIds = requestMatchIds(puuId, QueueType.RANKED_SOLO.getQueueId());
-		List<ParticipantDto> participants = extractParticipantsBy(puuId, matchIds);
+		String[] matchIds = requestMatchIds(puuId, QueueType.RANKED_SOLO.getQueueId());
+		List<MatchDto> matchDtos = requestMatchHistories(matchIds);
+		List<ParticipantDto> participants = extractParticipantsFrom(matchDtos, puuId);
 		List<ChampionStatDto> championStats = extractChampionStats(participants);
 		double latestWinRate = calculateLatestWinRate(participants);
 
 		return new RecentMatchStatsDto(latestWinRate, championStats);
 	}
 
-	private List<String> requestMatchIds(final String puuId, final int queueId) {
+	private String[] requestMatchIds(final String puuId, final int queueId) {
 		try {
 			return webClient.get()
 					.uri(MATCH_ID_REQUEST_URI, puuId, queueId, TOTAL_MATCH_COUNT, apiKey)
 					.retrieve()
-					.bodyToMono(new ParameterizedTypeReference<List<String>>() {
-					})
+					.bodyToMono(String[].class)
 					.block();
 		} catch (RuntimeException e) {
 			throw new InvalidPuuIdException();
 		}
 	}
 
-	private List<ParticipantDto> extractParticipantsBy(final String puuid, final List<String> recentMatchIds) {
-		return recentMatchIds.stream()
-				.map(this::requestMatchDto)
-				.map(MatchDto::getParticipants)
-				.map(participants -> findByPuuid(participants, puuid))
-				.collect(Collectors.toList());
+	private List<MatchDto> requestMatchHistories(final String[] matchIds) {
+		return Flux.fromArray(matchIds)
+				.flatMap(this::requestMatchMono)
+				.collectList()
+				.blockOptional()
+				.orElseThrow(InvalidMatchIdException::new);
 	}
 
-	private MatchDto requestMatchDto(final String matchId) {
+	private Mono<MatchDto> requestMatchMono(final String matchId) {
 		try {
 			return webClient.get()
 					.uri(MATCH_REQUEST_URI, matchId, apiKey)
 					.retrieve()
-					.bodyToMono(MatchDto.class)
-					.block();
+					.bodyToMono(MatchDto.class);
 		} catch (RuntimeException e) {
 			throw new InvalidMatchIdException();
 		}
+	}
+
+	private List<ParticipantDto> extractParticipantsFrom(final List<MatchDto> matchDtos, final String puuid) {
+		return matchDtos.stream()
+				.map(matchDto -> findByPuuid(matchDto.getParticipants(), puuid))
+				.collect(Collectors.toList());
 	}
 
 	private ParticipantDto findByPuuid(final List<ParticipantDto> participantDtos, final String puuid) {
